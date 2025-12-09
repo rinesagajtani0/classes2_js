8235c5c421ba7982773bcddc8d54999075bb10f8
rinesagajtani <rinesagajtani0@gmail.com>
2025-12-09 01:59:05 +0100

    first commit

---
+++ classes.js
@@ -0,0 +1,49 @@
+// Basic level:
+
+// Implement the User class. When creating an instance based on this class, the object must have the form {name: ‘Petro’, role: ‘admin’} (role can be either admin or user). In case of incorrectly transferred data of such an object, the corresponding field that was entered incorrectly should be warned using an alert. The User class must have the following components: 
+
+// getName
+// getRole
+// login
+// logout
+// сhangeName
+// changePassword
+// The Admin class should have the following components:
+
+// addUser
+// removeUser
+// changeUserRole
+// getAllUsers
+// removeAllUsers
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Intermediate level:
+
+// In the HTML page, add the ability for the user to create their own dashboard of clocks. These are clocks for different parts of the world. It is necessary to add an input field and a button to create a new clock when clicked. WorldClock is implemented via the class. Each instance of this class is a new clock. The class must have the following components:
+
+// getCurrentDate
+// getCurrentDateTime
+// deleteClock
+// button one – shows the user the time in text form
+// button two – displays the current date and time in text form
+// button three – deletes the clock from the "wall" of clocks
+
+// Mockup(a simple model)
